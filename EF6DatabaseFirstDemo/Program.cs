3a3989890b49b68d8fcdd72759b863177e19d2a4
kyo.lai82 <kyo.lai82@titansoft.com.sg>
2019-07-21 16:43:12 +0800

    Use generated class to insert a data into database

--- EF6DatabaseFirstDemo/Program.cs
+++ EF6DatabaseFirstDemo/Program.cs
@@ -1,21 +1,30 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace EF6DatabaseFirstDemo
 {
-	class Program
+	internal class Program
 	{
-		static void Main(string[] args)
+		private static void Main(string[] args)
 		{
 			// The code provided will print ‘Hello World’ to the console.
 			// Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
+			using (var ctx = new DatabaseFirstDemoEntities())
+			{
+				var employee = new Employee()
+				{
+					FirstName = "Kyo",
+					LastName = "Lai",
+					HireDate = new DateTime(2017,7,1),
+					Salary = 20000m
+				};
+				ctx.Employees.Add(employee);
+				ctx.SaveChanges();
+			}
+
 			Console.WriteLine("Hello World!");
 			Console.ReadKey();
 
-			// Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+			// Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app!
 		}
 	}
 }
